9ae128637219e0253893ba13aefcf20c384d8e15
Chris Ricker <chris.ricker@gmail.com>
2013-02-08 11:31:25 -0500

    Move OpenStack node plugins to nrpe module

--- manifests/init.pp
+++ manifests/init.pp
@@ -52,4 +52,105 @@ class nrpe(
     require => Package['nrpe_packages'],
   }
 
+
+  file { "/usr/lib/nagios/plugins/check_glance1":
+     notify  => Service["nrpe_service"],
+     ensure  => present,
+     owner   => "root",
+     group   => "root",
+     mode    => 0755,
+     source  => "puppet:///modules/nrpe/check_glance1",
+     require => Package["nrpe_packages"],
+  }
+
+    file { "/usr/lib/nagios/plugins/check_keystone":
+        notify  => Service["nrpe_service"],
+        ensure  => present,
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
+        source  => "puppet:///modules/nrpe/check_keystone",
+        require => Package["nrpe_packages"],
+    }
+
+    file { "/usr/lib/nagios/plugins/check_novaapi":
+        notify  => Service["nrpe_service"],
+        ensure  => present,
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
+        source  => "puppet:///modules/nrpe/check_novaapi",
+        require => Package["nrpe_packages"],
+    }
+
+    file { "/usr/lib/nagios/plugins/check_rabbitmq_aliveness":
+        notify  => Service["nrpe_service"],
+        ensure  => present,
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
+        source  => "puppet:///modules/nrpe/check_rabbitmq_aliveness",
+        require => Package["nrpe_packages"],
+    }
+
+    file { "/usr/lib/nagios/plugins/check_rabbitmq_objects":
+        notify  => Service["nrpe_service"],
+        ensure  => present,
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
+        source  => "puppet:///modules/nrpe/check_rabbitmq_objects",
+        require => Package["nrpe_packages"],
+    }
+
+    file { "/usr/lib/nagios/plugins/check_rabbitmq_overview":
+        notify  => Service["nrpe_service"],
+        ensure  => present,
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
+        source  => "puppet:///modules/nrpe/check_rabbitmq_overview",
+        require => Package["nrpe_packages"],
+    }
+
+    file { "/usr/lib/nagios/plugins/check_rabbitmq_server":
+        notify  => Service["nrpe_service"],
+        ensure  => present,
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
+        source  => "puppet:///modules/nrpe/check_rabbitmq_server",
+        require => Package["nrpe_packages"],
+    }
+
+    file { "/usr/lib/nagios/plugins/check_rabbitmq_queue":
+        notify  => Service["nrpe_service"],
+        ensure  => present,
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
+        source  => "puppet:///modules/nrpe/check_rabbitmq_queue",
+        require => Package["nrpe_packages"],
+    }
+
+    file { "/usr/lib/nagios/plugins/check_rabbitmq_watermark":
+        notify  => Service["nrpe_service"],
+        ensure  => present,
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
+        source  => "puppet:///modules/nrpe/check_rabbitmq_watermark",
+        require => Package["nrpe_packages"],
+    }
+
+    file { "/usr/lib/nagios/plugins/check_vm":
+        notify  => Service["nrpe_service"],
+        ensure  => present,
+        owner   => "root",
+        group   => "root",
+        mode    => 0755,
+        source  => "puppet:///modules/nrpe/check_vm",
+        require => Package["nrpe_packages"],
+    }
+
 }
